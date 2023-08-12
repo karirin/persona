792aaa7cee30e8acffe78cb0c29d0263a17a7149
karirin <karirin419@gmail.com>
2023-08-12 20:08:21 +0900

    Initial Commit

---
+++ persona/personaApp.swift
@@ -0,0 +1,17 @@
+//
+//  personaApp.swift
+//  persona
+//
+//  Created by hashimo ryoya on 2023/08/12.
+//
+
+import SwiftUI
+
+@main
+struct personaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
